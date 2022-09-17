c370c157552a41859c64ae61a1d6367a2cf80a7d
fazt <fazt@faztweb.com>
2022-09-17 01:55:14 -0500

    added db port to config

--- src/db.js
+++ src/db.js
@@ -1,9 +1,16 @@
 import { createPool } from "mysql2/promise";
-import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER } from "./config.js";
+import {
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+} from "./config.js";
 
 export const pool = createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
+  port: DB_PORT,
   database: DB_DATABASE,
 });
